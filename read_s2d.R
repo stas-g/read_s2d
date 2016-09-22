ba06665ee39bc1c4fcebb75c7802da2f9996a90f
Stasia Grinberg <stas-g@users.noreply.github.com>
2016-09-22 20:17:09 +0100

    Update read_s2d.R

--- read_s2d.R
+++ read_s2d.R
@@ -1,5 +1,5 @@
-read_s2d <- function(file_){
-	tmp <- readLines(file_)
+read_s2d <- function(file){
+	tmp <- readLines(file)
 	tmp <- split(tmp, cumsum(grepl('>', tmp)))
 	tmp <- lapply(tmp, FUN = function(x) do.call(rbind, strsplit(x, "\\t")))
 	names(tmp) <- sub("> ", "", sapply(tmp, "[[", 1, 1))
